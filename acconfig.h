ebe232627310fc25d067c2cd55c34897d8488271
Eddie Kohler <ekohler@gmail.com>
2002-07-08 16:06:58 +0000

    fix #undef inline

--- acconfig.h
+++ acconfig.h
@@ -33,7 +33,7 @@ char *strerror(int errno);
 
 #ifdef __cplusplus
 }
-/* Get rid of inline macro under C++ */
-# undef inline
+/* Get rid of a possible inline macro under C++. */
+# define inline inline
 #endif
 #endif
