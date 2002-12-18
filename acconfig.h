f18ea58589c42aad946b525275ab2272338e3430
Eddie Kohler <ekohler@gmail.com>
2002-12-18 09:23:21 +0000

    move to Oron Peled's t1utils.spec <oron@actcom.co.il>

--- acconfig.h
+++ acconfig.h
@@ -6,6 +6,15 @@
 #define PACKAGE "t1utils"
 #define VERSION "97"
 
+/* Define if you have the <inttypes.h> header file. */
+#undef HAVE_INTTYPES_H
+
+/* Define if you must include the <sys/types.h> header file. */
+#undef NEED_SYS_TYPES_H
+
+/* Define if you have u_intXX_t types but not uintXX_t types. */
+#undef HAVE_U_INT_TYPES
+
 @TOP@
 @BOTTOM@
 
@@ -19,17 +28,17 @@ char *strerror(int errno);
 #endif
 
 /* Get the [u]int*_t typedefs. */
-#undef NEED_SYS_TYPES_H
-#ifdef NEED_SYS_TYPES_H
-# include <sys/types.h>
-#endif
-#undef HAVE_INTTYPES_H
 #ifdef HAVE_INTTYPES_H
-# include <inttypes.h>
+#include <inttypes.h>
 #endif
-#undef uint16_t
-#undef uint32_t
-#undef int32_t
+#ifdef NEED_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+#ifdef HAVE_U_INT_TYPES
+typedef u_int8_t uint8_t;
+typedef u_int16_t uint16_t;
+typedef u_int32_t uint32_t;
+#endif
 
 #ifdef __cplusplus
 }
